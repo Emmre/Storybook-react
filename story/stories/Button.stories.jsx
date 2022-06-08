bd4bddf5e211304578c9b0416a72876565044ba9
emreesrc <emreesrc@gmail.com>
2022-06-08 20:09:47 +0300

    storybook

---
+++ story/stories/Button.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Button } from '@material-ui/core'
+
+export default {
+  title: 'Example/Button',
+  component: Button
+}
+
+export const Default = () => (
+  <div>
+    <Button variant='text'>Text</Button>
+  </div>
+)
